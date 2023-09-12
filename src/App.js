88db55c1353205400d368344f4b0dea9a8ac03a5
urfy1 <urfan.azim@gmail.com>
2023-09-12 15:51:38 +0100

    first d

--- src/App.js
+++ src/App.js
@@ -11,30 +11,23 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import { Card, CardHeader, CardBody, CardFooter, Heading, Button } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+
+<Card align='center'>
+  <CardHeader>
+    <Heading size='md'> Customer dashboard</Heading>
+  </CardHeader>
+  <CardBody>
+    <Text>View a summary of all your customers over the last month.</Text>
+  </CardBody>
+  <CardFooter>
+    <Button colorScheme='blue'>View here</Button>
+  </CardFooter>
+</Card>
     </ChakraProvider>
   );
 }

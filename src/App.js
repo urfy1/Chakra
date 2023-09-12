91f7daa9536cef0220d8d721a7c46dfd1b9c244b
urfy1 <urfan.azim@gmail.com>
2023-09-12 16:31:30 +0100

    first e

--- src/App.js
+++ src/App.js
@@ -1,33 +1,63 @@
 import React from 'react';
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+  ChakraProvider,Box,Text,Link,VStack,Code,Grid,theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
-import { Card, CardHeader, CardBody, CardFooter, Heading, Button } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Heading, Button, SimpleGrid, Container, Divider } from '@chakra-ui/react'
+
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
 
-<Card align='center'>
-  <CardHeader>
-    <Heading size='md'> Customer dashboard</Heading>
-  </CardHeader>
-  <CardBody>
-    <Text>View a summary of all your customers over the last month.</Text>
-  </CardBody>
-  <CardFooter>
-    <Button colorScheme='blue'>View here</Button>
-  </CardFooter>
-</Card>
+<Heading as='h1' size='4xl' noOfLines={1}>I'm a Heading</Heading>
+
+
+<Divider orientation='vertical' />
+
+
+<Container maxW='2xl' bg='blue.600' centerContent>
+
+<SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
+  <Card>
+    <CardHeader>
+      <Heading size='md'> Customer dashboard</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>View a summary of all your customers over the last month.</Text>
+    </CardBody>
+    <CardFooter>
+      <Button>View here</Button>
+    </CardFooter>
+  </Card>
+  <Card>
+    <CardHeader>
+      <Heading size='md'> Customer dashboard</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>View a summary of all your customers over the last month.</Text>
+    </CardBody>
+    <CardFooter>
+      <Button>View here</Button>
+    </CardFooter>
+  </Card>
+  <Card>
+    <CardHeader>
+      <Heading size='md'> Customer dashboard</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>View a summary of all your customers over the last month.</Text>
+    </CardBody>
+    <CardFooter>
+      <Button>View here</Button>
+    </CardFooter>
+  </Card>
+</SimpleGrid>
+
+</Container>
+
+
     </ChakraProvider>
   );
 }
---
+++ src/NewPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewPage = () => {
+  return (
+    <div>
+      <h1>New Page</h1>
+      hello
+    </div>
+  );
+};
+
+export default NewPage;
